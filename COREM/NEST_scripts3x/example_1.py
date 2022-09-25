391f89a4521a8b07f6d3bdcfaeb12a487e2cef58
Thang Duong <thang@quantee.com>
2022-09-25 02:15:15 -0700

    cleaned up by removing debug messages

--- COREM/NEST_scripts3x/example_1.py
+++ COREM/NEST_scripts3x/example_1.py
@@ -12,24 +12,19 @@ simTime = 1200.0
 
 # Reset kernel and network
 nest.ResetKernel()
-#nest.ResetNetwork()
 
 # Number of threads (must be 1) and resolution (the same of the retina script)
 nest.SetKernelStatus({"local_num_threads": 1,'resolution': 1.0})
 
 # Install module just once
-#model = nest.node_models(sel='corem')
-#model = nest.Models(mtype='nodes',sel='corem')
-#print(model)
-#if not model:
-nest.Install("corem_module")
-#exit(0)
+if 'corem' not in nest.node_models:
+    nest.Install("corem_module")
 
 # Create spike detector and spiking nodes
 mult = nest.Create('spike_recorder',1)
 spikingGanglion=nest.Create('iaf_psc_alpha',ganglionCells,{'E_L':-56.0})
 nest.Connect(spikingGanglion,mult)
-print(spikingGanglion)
+
 # COREM nodes
 for i in range(0,ganglionCells):
     g=nest.Create('corem',1,{'port':float(i),'filename':"Retina_scripts/example_1.py"})
